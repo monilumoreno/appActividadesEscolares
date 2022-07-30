3fc3ddc3d539cf12e02657bd7ae9d26942d60475
obenavides Local <obenavidesmt2022@gmail.com>
2022-07-30 00:05:40 -0500

    Actualización

--- documentacion/BD/03_script_consultas.sql
+++
@@ -1,9 +0,0 @@
-
-/*==============================================================*/
-/* Consultas
-/*==============================================================*/
-
-SELECT numerodocumento, nombres, apellidos, telefono, direccion, correo, usuario, clave, descripcionperfil, usuarios.estado
-FROM usuarios, perfiles WHERE usuarios.idperfil = perfiles.idperfil;
-
-SELECT * FROM alumnos, usuarios, cursos WHERE alumnos.idacudiente = usuarios.idusuario AND alumnos.idcurso = cursos.idcurso;
